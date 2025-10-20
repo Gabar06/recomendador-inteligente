8c9ac9461d86a17eb2904ec8e0e64daa0810f655
Fernando González <fergon.670@gmail.com>
2025-10-19 23:31:15 -0300

    Se modifica la conexion a db

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -92,15 +92,14 @@ ASGI_APPLICATION = 'core.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql', # Usa 'django.db.backends.postgresql' para PostgreSQL y 'django.db.backends.mysql' para MySQL
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://'+os.getenv('DB_USER')+':'+os.getenv('DB_PASSWORD')+'@localhost:5432/'+os.getenv('DB_NAME'),
+        conn_max_age=600
+    )
 }
 
 
